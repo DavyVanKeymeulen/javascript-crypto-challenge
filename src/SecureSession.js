379f2506544112be1affc8c1af019ead0151dd04
Davy <davy.vankeymeulen@hotmail.com>
2019-10-20 18:35:22 +0200

    Create SecureSession.js

---
+++ src/SecureSession.js
@@ -0,0 +1,72 @@
+const _sodium = require('libsodium-wrappers');
+var rx = null;
+var tx = null;
+var privateKey = null;
+var publicKey = null;
+var clientKey = null;
+
+module.exports.setClientPublicKey = function(key)
+{
+    /* client public key already set */
+    if (clientKey === key)
+        return;
+
+    /* does not allow us to modify our public key */
+    if ((clientKey !== null) && (clientKey !== key))
+        throw 'client public key already set';
+
+    clientKey = key;
+
+    /* Generate server key exchange keypair */
+    const keypair = _sodium.crypto_kx_keypair();
+    privateKey = keypair.privateKey;
+    publicKey = keypair.publicKey;
+
+    /* Generate shared keys */
+    sharedKeys = _sodium.crypto_kx_server_session_keys(publicKey,privateKey,key);  
+    
+    rx = sharedKeys.sharedRx;
+    tx = sharedKeys.sharedTx;
+    /* Set rx (shared secret key) & tx  send data and receive data 
+     The crypto_kx_server_session_keys() function computes a pair of shared keys (rx and tx) using 
+            the server's public key server_pk, 
+            the server's secret key server_sk and 
+            the client's public key client_pk.
+     It returns 0 on success, or -1 if the server's public key is not acceptable.
+     The shared secret key rx should be used by the server to receive data from the client, (by the server from client)
+     whereas tx should be used for data flowing in the opposite direction.(by the client from server)
+    */
+    
+}
+
+module.exports.serverPublicKey = async function()
+{
+    
+    await _sodium.ready;
+
+    /* Return public key */
+    return publicKey;
+}
+
+module.exports.encrypt = async function(msg)
+{
+ 
+    await _sodium.ready;
+
+    /* Generate nonce & encrypt */
+    /* https://libsodium.gitbook.io/doc/public-key_cryptography/authenticated_encryption*/
+    nonce = _sodium.randombytes_buf(_sodium.crypto_secretbox_NONCEBYTES)
+    ciphertext = _sodium.crypto_secretbox_easy(msg, nonce, tx)
+
+    /* Return cipher & nonce */
+    return { ciphertext, nonce }
+}
+
+module.exports.decrypt = async function(ciphertext, nonce)
+{
+    
+    await _sodium.ready;
+
+    /* Decrypt the message given then cipher & nonce, encoder */
+    return await _sodium.crypto_secretbox_open_easy(ciphertext, nonce, rx)
+}
